e6b314d77c02af4ef27533ed8d707a94ded6a882
SHIFU <tamangnischal2018@gmail.com>
2024-03-15 20:10:21 +0545

    creating home html

--- src/main/java/com/BookStoreLearning/controller/BookController.java
+++ src/main/java/com/BookStoreLearning/controller/BookController.java
@@ -1,8 +1,17 @@
 package com.BookStoreLearning.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
 
 }
+
+
