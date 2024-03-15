1fc004a3f3d3a75c2c083d1a0718fcda3694c065
SHIFU <tamangnischal2018@gmail.com>
2024-03-15 16:17:41 +0545

    first commit

---
+++ src/main/java/com/BookStoreLearning/BookStoreWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.BookStoreLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreWebAppApplication.class, args);
+	}
+
+}
