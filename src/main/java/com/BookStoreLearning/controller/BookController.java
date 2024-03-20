aa4ab086292beaf483672982f319f417269010e4
SHIFU <tamangnischal2018@gmail.com>
2024-03-20 17:34:04 +0545

    book save controller

--- src/main/java/com/BookStoreLearning/controller/BookController.java
+++ src/main/java/com/BookStoreLearning/controller/BookController.java
@@ -1,11 +1,17 @@
 package com.BookStoreLearning.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import com.BookStoreLearning.entity.Book;
+import com.BookStoreLearning.service.BookService;
 
 @Controller
 public class BookController {
 	
+	@Autowired
+	private BookService service;
 	@GetMapping("/")
 	public String home() {
 		return "home";
@@ -21,6 +27,14 @@ public class BookController {
 	public String getAllBook() {
 		return "bookList";
 	}
+	
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {
+		//TODO: process POST request
+		service.save(b);
+		return "redirect:/available_books";
+	}
+	
 
 }
 
