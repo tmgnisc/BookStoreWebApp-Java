1850f3bad0adee2f2eb00526041c456cd468b816
SHIFU <tamangnischal2018@gmail.com>
2024-03-21 17:54:01 +0545

    mybooks html delete icon

--- src/main/java/com/BookStoreLearning/controller/BookController.java
+++ src/main/java/com/BookStoreLearning/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -49,7 +50,10 @@ public class BookController {
 	}
 	
 	@GetMapping("/my_books")
-	public String getMyBooks() {
+	public String getMyBooks(Model model) {
+		
+		List<MyBookList>list = mybookService.getAllMyBooks();
+		model.addAttribute("book", list);
 		return "myBooks";
 	}
 	
