cb42fe5a8e80aeae7b48f3a60c522233ec9eb255
SHIFU <tamangnischal2018@gmail.com>
2024-03-21 20:16:39 +0545

    delete controller

---
+++ src/main/java/com/BookStoreLearning/controller/MyBookListController.java
@@ -0,0 +1,5 @@
+package com.BookStoreLearning.controller;
+
+public class MyBookListController {
+
+}
--- src/main/java/com/BookStoreLearning/service/MyBookListService.java
+++ src/main/java/com/BookStoreLearning/service/MyBookListService.java
@@ -22,4 +22,8 @@ public class MyBookListService {
 		return mybook.findAll();
 		
 	}
+	
+	public void deleteById(int id) {
+		mybook.deleteById(id);
+	}
 }
